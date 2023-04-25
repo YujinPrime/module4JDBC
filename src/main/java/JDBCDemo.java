fa4afb7afda1dbf6ad713ea4a9bd30fb25811ce2
Yevhenii Makovskyi <makofujin@gmail.com>
2023-04-25 19:16:58 +0300

    Initial commit

---
+++ src/main/java/JDBCDemo.java
@@ -0,0 +1,20 @@
+import database.service.DatabaseInitService;
+import database.service.DatabasePopulateService;
+import database.service.DatabaseQueryService;
+
+public class JDBCDemo {
+    public static void main(String[] args) {
+        DatabaseInitService databaseInitService = new DatabaseInitService();
+        databaseInitService.initDatabase();
+
+        DatabasePopulateService databasePopulateService = new DatabasePopulateService();
+        databasePopulateService.populateDatabase();
+
+        DatabaseQueryService databaseQueryService = new DatabaseQueryService();
+        System.out.println(databaseQueryService.findMaxSalaryWorker());
+        System.out.println(databaseQueryService.findMaxProjectsClient());
+        System.out.println(databaseQueryService.findLongestProject());
+        System.out.println(databaseQueryService.findYoungestEldestWorkers());
+        System.out.println(databaseQueryService.findProjectPrices());
+    }
+}
